430f765c1f58a46a2363c4b6e24f238f9fab982d
Chong Cao <caochong2000@gmail.com>
2024-09-19 14:38:53 -0700

    Update mux10.v
    Updated comments

--- mux10.v
+++ mux10.v
@@ -1,4 +1,4 @@
-
+//Multiplexer module checks select input in order to determine whether to display results of addition/subtraction or multiplication of the two input 4-bit values
 module mux10 (
 	       input [9:0] in0, in1,
 	       input sel,
