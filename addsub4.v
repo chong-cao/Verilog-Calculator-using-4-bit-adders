f44fe44fced50a6f10a29c382954d92fad2d40a6
Chong Cao <caochong2000@gmail.com>
2024-09-19 14:36:26 -0700

    Update addsub4.v
    Updated comments

--- addsub4.v
+++ addsub4.v
@@ -1,17 +1,20 @@
+//New module utilizing the add4 module to perform either addition or subtraction by changing the second 4-bit input into its 2s complement before addition
 module addsub4 (
            input [3:0] A, B,
-           input subsel,
+           input subsel, //Input subsel will change B into its 2s complment when subsel = 1
            output [3:0] X,
            output cout, ovf
            );
-wire [3:0] C;
 
+//Declares wire C, which XORs every element in input B with subsel to see if B's value needs to be converted to 2s complement for subtraction           
+wire [3:0] C;
 assign C[0] = B[0]^subsel;
 assign C[1] = B[1]^subsel;
 assign C[2] = B[2]^subsel;
 assign C[3] = B[3]^subsel;
-add4 addsubstage1(.carryin(subsel), .X(A), .Y(C), .S(X), .carryout(cout), .ovf(ovf));
 
+//subsel is included in the carryin portion in order to add 1 to C so it can become 2s complement
+add4 addsubstage1(.carryin(subsel), .X(A), .Y(C), .S(X), .carryout(cout), .ovf(ovf));
 
 
 endmodule
