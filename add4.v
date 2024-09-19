7a90352f61f4666057b7fff0fa3e0552fdfd006b
Chong Cao <caochong2000@gmail.com>
2024-09-19 14:32:40 -0700

    Update add4.v
    Added descriptions

--- add4.v
+++ add4.v
@@ -1,6 +1,6 @@
-
+//Base 4-bit adder module, accounts for addtion of two 4-bit values as well as carry + overflow
 module add4 (
-	     input	      carryin,
+	     input	      carryin, 
 	     input [3:0]      X, Y,
 	     output reg [3:0] S,
 	     output reg	      carryout,
@@ -9,11 +9,13 @@ module add4 (
    
    reg [4:0]		      C;
    integer		      k;
-   
+
+	//Always block performs operation any time the two 4-bit values or carryin are modified
    always @(X, Y, carryin)
      begin
 	C[0] = carryin;
 	for (k = 0; k < 4; k = k+1)
+	//For loop goes thru each bit individually and uses logic to perform addition while accounting for carries
 	  begin
 	     S[k] = X[k] ^ Y[k] ^ C[k];
 	     C[k+1] = (X[k] & Y[k]) | (X[k] & C[k]) | (Y[k] & C[k]);
